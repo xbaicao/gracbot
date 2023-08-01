15cf63b720fe0546011fd12ac83eeb42a19e78ba
百草 <1424716594@qq.com>
2023-08-01 15:37:07 +0000

    rename rm.sh to deletegit.sh.
    Signed-off-by: 百草 <1424716594@qq.com>

--- deletegit.sh
+++ deletegit.sh
@@ -2,9 +2,9 @@
 
 #初始化加载
 if [ -f "$HOME/.Yunzai" ]; then
-  echo -e "\033[32m校验成功\033[0m"
+  echo -e "\033[34m校验成功\033[0m"
 else
-  echo -e "\033[33m初始化文件中\033[0m"
+  echo -e "\033[34m初始化文件中\033[0m"
   sleep 0.3
   echo "/root/Yunzai-Bot" > "$HOME/.Yunzai"
 fi
@@ -34,18 +34,18 @@ EXCLUDED_DIRS=("example" "genshin" "system" "other" "bin")
 
     # 用户选择要删除的序号并确认
     if [ ${#plugin_dirs[@]} -eq 0 ]; then
-    echo -e "\e[31m未找到要删除的插件\e[0m"
+    echo -e "\e[34m未找到要删除的插件\e[0m"
     exit 1
     fi
 
 
-echo -e "\n\e[1;34m请输入你要删除的目录的编号，多个请用空格隔开: \e[0m"
-        echo -e "===================================================\e[1;32m"
+echo '复制粘贴需要删除的插件序号，多个请用空格隔开：'
+        echo 
 read -r chosen_indexes
 chosen_indexes=($chosen_indexes)
 
 if [ ${#chosen_indexes[@]} -eq 0 ]; then
-    echo -e "\e[0m\e[33m未选择任何要删除的目录\n"
+    echo -e "\e[0m\e[34m未选择任何要删除的目录\n"
     exit 1
 fi
 
@@ -55,31 +55,31 @@ for chosen_index in "${chosen_indexes[@]}"; do
     if [ -d "$chosen_dir" ]; then
         chosen_dirs+=("$chosen_dir")
     else
-        echo -e "%s \e[0m\e[31m目录不存在\n" "$chosen_dir"
+        echo -e "%s \e[0m\e[34m目录不存在\n" "$chosen_dir"
     fi
 done
 
 if [ ${#chosen_dirs[@]} -eq 0 ]; then
-    echo -e "\e[31m未找到任何要删除的目录\e[0m\n"
+    echo -e "\e[34m未找到任何要删除的目录\e[0m\n"
     exit 1
 fi
 
 clear
-printf "\n\e[0m\e[36m你将要删除以下目录：\e[0m\n"
+printf "\n\e[0m\e[34m你将要删除以下目录：\e[0m\n"
 for chosen_dir in "${chosen_dirs[@]}"; do
     printf "%s\n" "$chosen_dir"
 done
 
-echo -e "\n是否继续删除？\e[33m[Y/N]\e[0m "
+echo -e "\n是否继续删除？\e[34m[Y/N]\e[0m "
 read -r confirm
 if [[ ! $confirm =~ ^[Yy]$ ]]; then
-    echo -e "\e[33m删除操作已取消\e[0m"
+    echo -e "\e[34m删除操作已取消\e[0m"
     exit 0
 fi
 
 # 删除选中的目录
 for chosen_dir in "${chosen_dirs[@]}"; do
-    echo -e "\e[36m正在删除插件\e[0m %s ...\n" "$chosen_dir"
+    echo -e "\e[34m正在删除插件\e[0m %s ...\n" "$chosen_dir"
     rm -rf "$chosen_dir"
     printf "\n%s 已被永久删除\n" "$chosen_dir"
 done
