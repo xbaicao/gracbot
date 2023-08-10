b4d7e257ab7c3aa4d9d52cc7795d9d1997b1a00c
百草 <1424716594@qq.com>
2023-08-10 10:45:08 +0000

    自定义根目录路径
    Signed-off-by: 百草 <1424716594@qq.com>

--- caoyz.sh
+++ caoyz.sh
@@ -944,13 +944,13 @@ echo -e "--------------------------------\e[1;32m"
 dirs=$(find "${homedir}" -maxdepth 1 -type d ! -name ".*" -exec test -f '{}/lib/bot.js' ';' -print | sort)
 
 # 输出所有 Yunzai-BOT 根目录名，并使用 select 提供选择菜单
-PS3="请选择一个目录（输入 # 或 q 退出）："
+PS3="请选择一个目录（输入数字）（输入-退出）："
 select dir in ${dirs}; do
   case "$dir" in
     "")
       echo -e "\e[31m无效的目录编号，请重试！\e[0m"
       ;;
-    "#"|"q")
+    -)
       echo "\e[32m已退出，未作更改\e[0m" > "${homedir}/.Yunzai"
       exit
       ;;
